be4739c30d541094c7f8488ffef886d3444020b5
Aloïs Deniel <alois.deniel@gmail.com>
2015-08-08 11:14:49 +0200

    Customized theme with settings colors.

--- Mobile.Metrics/Mobile.Metrics/Reporting/HtmlReporter.cs
+++ Mobile.Metrics/Mobile.Metrics/Reporting/HtmlReporter.cs
@@ -19,11 +19,19 @@ namespace Mobile.Metrics.Reporting
 
         }
 
-        public void ApplyTemplates(string output, string content, string solution)
+        private string ApplyTemplates(string content, string solution)
         {
             content = content.Replace("{{SOLUTION}}", solution);
             content = content.Replace("{{Settings.Name}}", Settings.Global.Name);
+            content = content.Replace("{{Settings.AccentColor}}", Settings.Global.Colors[0]);
             content = content.Replace("{{Settings.Colors}}", String.Format("[\"{0}\"]", String.Join("\",\"", Settings.Global.Colors)));
+
+            return content;
+        }
+
+        private void SaveTemplates(string output, string content, string solution)
+        {
+            content = this.ApplyTemplates(content, solution);
 
             File.Delete(output);
 
@@ -36,17 +44,30 @@ namespace Mobile.Metrics.Reporting
             var solution = metrics.Metrics.Name.Replace(".sln",String.Empty);
 
             var htmlOutput = output + solution + ".html";
+            var cssOutput = output + solution + ".css";
             var jsOutput = output + solution + ".Mobile.Metrics.js";
             var dataOutput = output + solution + ".Data.js";
             
-
             var jsContent = File.ReadAllText("./Views/Js/Mobile.Metrics.js");
             var dataContent = String.Format("var jsonReport = '{0}'; console.log(jsonReport); var report = JSON.parse(jsonReport);", json);
             var htmlContent = File.ReadAllText("./Views/Html/Mobile.Metrics.html");
 
-            ApplyTemplates(htmlOutput, htmlContent, solution);
-            ApplyTemplates(dataOutput, dataContent, solution);
-            ApplyTemplates(jsOutput, jsContent, solution);
+            // Can't get libsassnet or dotless to work so ugly workaround ...
+            var cssContent = File.ReadAllText("./Views/Css/bootswatch.css");
+            cssContent = cssContent
+                .Replace("../fonts/", "http://bootswatch.com/fonts/")
+                .Replace("#165ba8", "#444444")
+                .Replace("#10427b", "#555555")
+                .Replace("#1862b5", "{{Settings.AccentColor}}")
+                .Replace("#1967be", "{{Settings.AccentColor}}")
+                .Replace("#2780e3", "{{Settings.AccentColor}}");
+            
+
+            SaveTemplates(htmlOutput, htmlContent, solution);
+            SaveTemplates(dataOutput, dataContent, solution);
+            SaveTemplates(jsOutput, jsContent, solution);
+            SaveTemplates(cssOutput, cssContent, solution);
+
         }
     }
 }
